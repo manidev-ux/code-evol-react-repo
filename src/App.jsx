e045a327c516fd37bce8fff2a95b06e56acbdf65
Mani Kumar <mani.malle.dev@gmail.com>
2026-04-10 10:41:30 +0530

    state commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import FunSt from "./State/FunSt";
-import ClsSt from "./State/ClsSt";
-import CounterFun from "./State/CounterFun";
-import CounterCls from "./State/CounterCls";
+import FunSt from "./components/Functional/State/FunSt";
+import ClsSt from "./components/Functional/State/ClsSt";
+import CounterFun from "./components/Functional/State/CounterFun";
+import CounterCls from "./components/Functional/State/CounterCls";
 
 function App() {
   return (
